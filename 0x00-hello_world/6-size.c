770a463a62ee2a16f50e28e78cf40153fead8ad7
bayo <bayobello767@gmail.com>
2023-06-17 09:00:19 -0700

    0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of short: %zu byte(s)\n", sizeof(short));
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
