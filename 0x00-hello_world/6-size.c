6a6479bb89a1cbcc12f0b3dff148484cb556ee66
bayo <bayobello767@gmail.com>
2023-06-17 08:29:58 -0700

    0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,20 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: 0 (success)
+ * main - prints the size of various types based on
+ * the computer it is compiled and run on...
+ * Return: Always 0.
  */
-int main() {
-    printf("Size of char: %zu byte(s)\n", sizeof(char));
-    printf("Size of short: %zu byte(s)\n", sizeof(short));
-    printf("Size of int: %zu byte(s)\n", sizeof(int));
-    printf("Size of long: %zu byte(s)\n", sizeof(long));
-    printf("Size of long long: %zu byte(s)\n", sizeof(long long));
-    printf("Size of float: %zu byte(s)\n", sizeof(float));
-    printf("Size of double: %zu byte(s)\n", sizeof(double));
-    printf("Size of long double: %zu byte(s)\n", sizeof(long double));
-    return 0;
+
+int main(void)
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of short: %zu byte(s)\n", sizeof(short));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of long: %zu byte(s)\n", sizeof(long));
+	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of double: %zu byte(s)\n", sizeof(double));
+	printf("Size of long double: %zu byte(s)\n", sizeof(long double));
+	return (0);
+}
