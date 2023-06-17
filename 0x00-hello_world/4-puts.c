7d7e34378709585408f4b959af0b7ab1134f8833
bayo <bayobello767@gmail.com>
2023-06-17 06:10:53 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle, followed by a newline."
  *
- * Return: 0.
+ * Return: Always 0 .
  */
 
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+    return 0);
 }
