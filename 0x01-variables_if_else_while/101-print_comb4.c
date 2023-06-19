8f778dfe9664f382ee30e23238e341cbbd1520b2
bayo <bayobello767@gmail.com>
2023-06-19 15:46:51 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,29 +8,29 @@
 
 int main(void)
 {
-    int d1, d2, d3;
+int d1, d2, d3;
 
-    for (d1 = 0; d1 <= 7; d1++)
-    {
-        for (d2 = d1 + 1; d2 <= 8; d2++)
-        {
-            for (d3 = d2 + 1; d3 <= 9; d3++)
-            {
-                putchar('0' + d1);
-                putchar('0' + d2);
-                putchar('0' + d3);
+for (d1 = 0; d1 <= 7; d1++)
+{
+for (d2 = d1 + 1; d2 <= 8; d2++)
+{
+for (d3 = d2 + 1; d3 <= 9; d3++)
+{
+putchar('0' + d1);
+putchar('0' + d2);
+putchar('0' + d3);
 
-                if (d1 != 7 || d2 != 8 || d3 != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+if (d1 != 7 || d2 != 8 || d3 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
 
