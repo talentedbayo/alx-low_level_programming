89d15ceb357dbd8f0af5bd7438eb2939102e39ee
bayo <bayobello767@gmail.com>
2023-06-19 12:48:06 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lower case and upper case
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{       
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+        
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}
