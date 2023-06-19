52fada6a933319d2d2e4b81f4825f9bccbc1fa76
bayo <bayobello767@gmail.com>
2023-06-19 09:00:52 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ n = rand() - RAND_MAX / 2;
 lastd = n % 10;
 if (lastd > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n,lastd);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 }
 else if (lastd == 0)
 {
